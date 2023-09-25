9beb63c07155f650ef182a2128440c9d86d257d9
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-25 22:03:37 +0200

    x
    x

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+
+
+import '../stylesheet/details.css'
+import { motion } from "framer-motion"
 
 const Details = () => {
+  
+
   return (
-    <div>Details</div>
+
+
+
+    <>
+
+      <div className='Section-details'>
+        <div className='title'>
+          <motion.h2 
+          initial={{scale: 0}}
+          animate={{scale: 1}}
+          transition={{
+            duration: 3,
+            ease: 'easeInOut'
+          }}
+          
+          >Hey there...</motion.h2>
+        </div>
+        </div>
+       
+      
+
+
+
+    </>
   )
 }
 
