a728fa09bea09bf5a4266374b3cec0a454618dcb
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-26 19:22:44 +0200

    update

---
+++ src/images/assets.js
@@ -0,0 +1,8 @@
+import image1 from "../images/imag-1.jpg";
+import image2 from "../images/imag-2.jpg";
+import image3 from "../images/imag-3.jpg";
+import image4 from "../images/imag-4.jpg";
+import image5 from "../images/imag-5.jpg";
+import image6 from "../images/imag-6.jpg";
+
+export default [image1, image2, image3, image4, image5, image6];
--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,24 +1,39 @@
-
-
 import '../stylesheet/details.css'
 import { motion } from "framer-motion"
+import { useRef, useEffect, useState } from 'react'
+import image from '../images/assets'
+import assets from '../images/assets'
+
 
 
 
 const Details = () => {
  
-  
+ 
 
   return (
 
 
-
     <>
 
-      <div className='Section-details'>
+      <div className='container-details'>
        
-        <h2>abaut</h2>
+        <motion.h4 
+        animate={{x: 250}}
+        transition={{transition: 'ease-in-out', duration: 3}}
+        >Gallery</motion.h4>
         </div>
+        <motion.div className='carousel'>
+          <motion.div drag='x' className='inner-carousel'>
+            {image.map((image) =>{
+              return (
+                <motion.div className='item-imag'>
+                  <img src={image} alt='bilder' />
+                </motion.div>
+              );
+            })}
+          </motion.div>
+        </motion.div>
        
       
 
