8caf2954d1657b558b61fbadc2e0c5e1388e9318
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-30 09:41:21 +0200

    update
    added  typewriter effect
    added neon style on title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,6 @@
 import '../stylesheet/card.css';
 import { motion } from "framer-motion"
+import Typewriter from 'typewriter-effect';
 
 
 const Card = () => {
@@ -11,7 +12,15 @@ const Card = () => {
         whileHover={{ scale: 1.1 }}
         whileTap={{ scale: 0.9 }}
         className='cont-dragcard'>
-          <h3>This is a simple Card</h3>
+          <h3><Typewriter
+  options={{
+    strings: ['Simple', 'Card', 'Drag', 'and Hover Efect', 'using Framer Motion'],
+    autoStart: true,
+    
+    loop: true,
+    pauseFor: 1500,
+  }}
+/></h3>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi accusamus in voluptate asperiores amet nulla pariatur vero nisi provident consequuntur tempora sequi, sunt id? Ratione eos cupiditate magnam laudantium voluptates.</p>
         <motion.div
           className='dragcard'
