1ed6b166ed064b6f37dc95b650f19080de92f4ef
diego-monfort-landolt <diegomonfort1989@gmail.com>
2023-09-30 20:37:42 +0200

    updadet
    added animation on scroll homepage

---
+++ src/components/scroll.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus similique placeat sint hic maxime, cumque qui vitae magni consequuntur saepe rem molestiae et non nesciunt minima rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus similique placeat sint hic maxime, cumque qui vitae magni consequuntur saepe rem molestiae et non nesciunt minima rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus similique placeat sint hic maxime, cumque qui vitae magni consequuntur saepe rem molestiae et non nesciunt minima rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus similique placeat sint hic maxime, cumque qui vitae magni consequuntur saepe rem molestiae et non nesciunt minima rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus similique placeat sint hic maxime, cumque qui vitae magni consequuntur saepe rem molestiae et non nesciunt minima rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus similique placeat sint hic maxime, cumque qui vitae magni consequuntur saepe rem molestiae et non nesciunt minima rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus similique placeat sint hic maxime, cumque qui vitae magni consequuntur saepe rem molestiae et non nesciunt minima rerum?Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, ipsum dolor! Minus';
+
+const scroll = () => {
+
+
+  return (
+    <>
+      <div className='about-title'>
+        <h2>Abaut</h2>
+
+        <h3>Here we Start whit Framer Motion animacion...</h3>
+        <p>{txt}</p>
+        <h3>Here we Start whit Framer Motion animacion...</h3>
+        <p>{txt}</p>
+        <h3>Here we Start whit Framer Motion animacion...</h3>
+        <p>{txt}</p>
+        <h3>Here we Start whit Framer Motion animacion...</h3>
+        <p>{txt}</p>
+
+      </div>
+
+
+    </>
+  )
+}
+
+export default scroll
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import '../stylesheet/abaout.css';
+import Scroll from '../components/scroll'
 
 const About = () => {
   return (
     <>
       <div className='about-container'>
-        <div className='about-title'>
-          <h2>Abaut</h2>
-
-          <h3>Here we Start whit Framer Motion animacion...</h3>
-        </div>
+        <Scroll />
 
 
 
